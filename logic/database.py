6363b61eb69cb177b367895eef4a6de761075faf
Kiruha01 <liss-2001g@mail.ru>
2021-03-25 02:41:09 +0300

    Fix with NoneType error

--- logic/database.py
+++ logic/database.py
@@ -10,7 +10,7 @@ session = DBSession()
 
 class get_user(object):
     def __new__(cls, telegram_id) -> User:
-        if (hasattr(cls, 'user') and cls.user.telegram_id != telegram_id) or not hasattr(cls, 'user'):
+        if (hasattr(cls, 'user') and (cls.user is not None) and cls.user.telegram_id != telegram_id) or not hasattr(cls, 'user'):
             cls.user = session.query(User).filter_by(telegram_id=telegram_id).first()
         return cls.user
 
