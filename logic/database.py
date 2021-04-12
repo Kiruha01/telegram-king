afd4d7d6139f19b7f5a7ade1583817c19758991c
PythonAnyWhere <game@pythonanywhere.com>
2021-04-12 08:33:24 +0000

    Add exceptions handler onto rollback:

--- logic/database.py
+++ logic/database.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import PendingRollbackError
 
 from logic.db_setup import Player, User, Base, engine
 
@@ -43,31 +44,49 @@ points_for_4 = {
 class get_user(object):
     def __new__(cls, telegram_id) -> User:
         if (hasattr(cls, 'user') and (cls.user is not None) and cls.user.telegram_id != telegram_id) or not hasattr(cls, 'user'):
-            cls.user = session.query(User).filter_by(telegram_id=telegram_id).first()
+            try:
+                cls.user = session.query(User).filter_by(telegram_id=telegram_id).first()
+            except PendingRollbackError:
+                session.rollback()
         return cls.user
 
 
 def add(obj):
-    session.add(obj)
+    try:
+        session.add(obj)
+    except PendingRollbackError:
+        session.rollback()
 
 def commit():
-    session.commit()
+    try:
+        session.commit()
+    except PendingRollbackError:
+        session.rollback()
 
 
 def get_players(creator_id):
-    return session.query(Player).filter_by(creator=creator_id)
+    try:
+        return session.query(Player).filter_by(creator=creator_id)
+    except PendingRollbackError:
+        session.rollback()
 
 
 # TODO: Удалить после создания всех шагов бота
 def del_all_players():
-    players = session.query(Player).all()
-    for i in players:
-        session.delete(i)
-    session.commit()
+    try:
+        players = session.query(Player).all()
+        for i in players:
+            session.delete(i)
+        session.commit()
+    except PendingRollbackError:
+        session.rollback()
 
 
 def del_players_by_creator(creator):
-    players = session.query(Player).filter_by(creator=creator)
-    for i in players:
-        session.delete(i)
-    session.commit()
+    try:
+        players = session.query(Player).filter_by(creator=creator)
+        for i in players:
+            session.delete(i)
+        session.commit()
+    except PendingRollbackError:
+        session.rollback()
